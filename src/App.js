c25b998e00005fa7217e97ada5fa9ec403378e50
Zleepless <donutsepia@gmail.com>
2019-07-30 11:31:22 +0700

    Add Info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Header } from './components/common';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
+import { thisExpression } from '@babel/types';
 
 class App extends Component {
+    state = { loggedIn: false };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyD3dDT28YMX6XgXdZfs1MTEKjNI68A0Bo8',
@@ -14,13 +18,36 @@ class App extends Component {
             messagingSenderId: '325630541773',
             appId: '1:325630541773:web:7b54cd485c209020'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Button onPress={() => firebase.auth().signOut()}>
+                        Log Out
+                    </Button>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large" />;
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {thisExpression.renderContent()}
             </View>
         );
     }
